7a86c3e42bf2d4d8d635f2b483b2954ba839675c
Paulo Grabin <plgrabin@gmail.com>
2025-04-17 17:57:56 -0300

    Move the module declaration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module com.paulograbin.modules {
 
     requires java.net.http;
+    requires java.logging;
+    requires lwjgl;
+    requires lwjgl.util;
 
     exports com.paulograbin.modules.sitecheck;
     exports com.paulograbin.modules.sitecheck.concurrent;
+
+    exports com.paulograbin.openGL.javaGL;
 
 }
