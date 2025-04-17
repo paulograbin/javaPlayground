9103a39c31c75b65d49939f9d790b046002e0e5d
Paulo Grabin <plgrabin@gmail.com>
2025-04-17 15:52:14 -0300

    Add tests for Regexes

---
+++ src/test/java/com/paulograbin/RegexTests.java
@@ -0,0 +1,45 @@
+package com.paulograbin;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class RegexTests {
+
+    final Pattern pattern = Pattern.compile("^\\w+,\\d+$");
+//    final Pattern pattern = Pattern.compile(".*,[0-9]*");
+
+    @Test
+    public void name() {
+        assertResult("5054772120812,4", true);
+        assertResult("5053744478128,3", true);
+        assertResult("123123123123,2", true);
+        assertResult("300617127,1", true);
+        assertResult("300412426,2", true);
+        assertResult("aaaaaaaa,2", true);
+        assertResult("aaaaa,1111", true);
+        assertResult("5054772120812,4", true);
+        assertResult("5054773239582,1", true);
+
+        assertResult("aaaaa", false);
+        assertResult("5054773239582,adada1424242", false);
+        assertResult("300412426-2", false);
+        assertResult("aaaaa", false);
+        assertResult("aaaaa,aaaaaa", false);
+        assertResult("5054773239582,adada1", false);
+        assertResult("aaaaaaaa,2543aa", false);
+        assertResult("sadasd$#,2543aa", false);
+        assertResult("sadasd$#+2543aa", false);
+        assertResult("300412426+2", false);
+    }
+
+    private void assertResult(String stringToTest, boolean expectedResult) {
+        Matcher matcher = pattern.matcher(stringToTest);
+        boolean result = matcher.find();
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+}
