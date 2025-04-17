3c4083537fe7d4d247704b2ab89622b51398b76e
Paulo Grabin <plgrabin@gmail.com>
2025-04-17 15:52:50 -0300

    Add tests for Apache Commons lang3 random strings

---
+++ src/test/java/com/paulograbin/RandomStringUtilsTest.java
@@ -0,0 +1,53 @@
+package com.paulograbin;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+public class RandomStringUtilsTest {
+
+    @Test
+    public void test() {
+        System.out.println(RandomStringUtils.randomPrint(20));
+        System.out.println(RandomStringUtils.random(20));
+        System.out.println(RandomStringUtils.random(20));
+        System.out.println(RandomStringUtils.random(20));
+        System.out.println();
+
+        System.out.println(RandomStringUtils.randomAscii(20));
+        System.out.println(RandomStringUtils.randomAscii(20));
+        System.out.println(RandomStringUtils.randomAscii(20));
+        System.out.println(RandomStringUtils.randomAscii(20));
+        System.out.println();
+
+        System.out.println(RandomStringUtils.randomGraph(20));
+        System.out.println(RandomStringUtils.randomGraph(20));
+        System.out.println(RandomStringUtils.randomGraph(20));
+        System.out.println(RandomStringUtils.randomGraph(20));
+        System.out.println();
+
+        System.out.println(RandomStringUtils.randomAlphabetic(20));
+        System.out.println(RandomStringUtils.randomAlphabetic(20));
+        System.out.println(RandomStringUtils.randomAlphabetic(20));
+        System.out.println(RandomStringUtils.randomAlphabetic(20));
+        System.out.println();
+
+        System.out.println(RandomStringUtils.random(20, false, false));
+        System.out.println(RandomStringUtils.random(20, false, false));
+        System.out.println(RandomStringUtils.random(20, false, false));
+        System.out.println(RandomStringUtils.random(20, false, false));
+        System.out.println();
+
+        System.out.println(RandomStringUtils.random(20, true, false));
+        System.out.println(RandomStringUtils.random(20, true, false));
+        System.out.println(RandomStringUtils.random(20, true, false));
+        System.out.println(RandomStringUtils.random(20, true, false));
+        System.out.println();
+
+        System.out.println(RandomStringUtils.random(20, true, true));
+        System.out.println(RandomStringUtils.random(20, true, true));
+        System.out.println(RandomStringUtils.random(20, true, true));
+        System.out.println(RandomStringUtils.random(20, true, true));
+        System.out.println();
+    }
+
+}
