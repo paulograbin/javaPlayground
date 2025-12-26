7be34ecfcc387d425998a11967c99d818ff5e47f
Paulo Grabin <paulo.grabin@sap.com>
2025-12-26 09:48:22 -0300

    FizzBuzz

---
+++ src/main/java/com/paulograbin/leetcode/FizzBuzz.java
@@ -0,0 +1,31 @@
+package com.paulograbin.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public List<String> fizzBuzz(int n) {
+        if (n == 0) {
+            return Collections.emptyList();
+        }
+
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                answer.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                answer.add("Fizz");
+            } else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else {
+                answer.add(i + "");
+            }
+        }
+
+        return answer;
+    }
+
+}
---
+++ src/test/java/com/paulograbin/leetcode/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+package com.paulograbin.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+    private final FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    void name() {
+        System.out.println(fizzBuzz.fizzBuzz(3));
+    }
+
+    @Test
+    void name2() {
+        System.out.println(fizzBuzz.fizzBuzz(1));
+    }
+}
