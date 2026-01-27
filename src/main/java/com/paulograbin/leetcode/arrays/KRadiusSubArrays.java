7e348232f2e1e6b867706033dd79e370cec56e53
Paulo Grabin <paulo.grabin@sap.com>
2026-01-27 16:03:18 -0300

    Checkpoint

--- src/main/java/com/paulograbin/leetcode/arrays/KRadiusSubArrays.java
+++ src/main/java/com/paulograbin/leetcode/arrays/KRadiusSubArrays.java
@@ -3,9 +3,34 @@ package com.paulograbin.leetcode.arrays;
 public class KRadiusSubArrays {
 
     public int[] getAverages(int[] nums, int k) {
-        int[] a = new int[]{0};
+        int[] averages = new int[nums.length];
+        for (int i = 0; i < averages.length; i++) {
+            averages[i] = -1;
+        }
 
-        return a;
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+
+        if (k == 0) {
+            return nums;
+        }
+
+        for (int i = k; i < sum.length - k; i++) {
+            long currentSum;
+            if (i - k - 1 < 0) {
+                currentSum = sum[i + k];
+            } else {
+                currentSum = sum[i + k] - sum[i - k];
+            }
+
+            long currentAverage = currentSum / ((2L * k) + 1);
+            averages[i] = (int) currentAverage;
+        }
+
+        return averages;
     }
 
 }
--- src/test/java/com/paulograbin/leetcode/arrays/KRadiusSubArraysTest.java
+++ src/test/java/com/paulograbin/leetcode/arrays/KRadiusSubArraysTest.java
@@ -1,7 +1,8 @@
 package com.paulograbin.leetcode.arrays;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class KRadiusSubArraysTest {
 
@@ -9,29 +10,41 @@ class KRadiusSubArraysTest {
 
     @Test
     void basic() {
-        int[] nums = new int[]{8};
+        int[] nums = new int[]{7, 4, 3, 9, 1, 8, 5, 2, 6};
 
-        int[] averages = sub.getAverages(nums, 8);
+        int[] averages = sub.getAverages(nums, 3);
+        assertThat(averages).isEqualTo(new int[]{-1, -1, -1, 5, 4, 4, -1, -1, -1});
+    }
 
-        Assertions.assertThat(averages).isEqualTo(new int[]{-1});
+    @Test
+    void testCase() {
+        int[] nums = new int[]{18334, 25764, 19780, 92480, 69842, 73255, 89893};
+
+        int[] averages = sub.getAverages(nums, 0);
+        assertThat(averages).isEqualTo(new int[]{18334, 25764, 19780, 92480, 69842, 73255, 89893});
+    }
+
+    @Test
+    void testCase2() {
+        int[] nums = new int[]{40527, 53696, 10730, 66491, 62141, 83909, 78635, 18560};
+
+        int[] averages = sub.getAverages(nums, 2);
+        assertThat(averages).isEqualTo(new int[]{-1, -1, 46717, 55393, 60381, 61947, -1, -1});
     }
 
     @Test
     void anotjher() {
-        int[] nums = new int[]{8};
+        int[] nums = new int[]{100000};
 
-        int[] averages = sub.getAverages(nums, 8);
-
-        Assertions.assertThat(averages).isEqualTo(new int[]{-1});
+        int[] averages = sub.getAverages(nums, 0);
+        assertThat(averages).isEqualTo(new int[]{100000});
     }
 
     @Test
     void yetAnother() {
         int[] nums = new int[]{8};
 
-        int[] averages = sub.getAverages(nums, 8);
-
-        Assertions.assertThat(averages).isEqualTo(new int[]{-1});
-
+        int[] averages = sub.getAverages(nums, 100000);
+        assertThat(averages).isEqualTo(new int[]{-1});
     }
 }
