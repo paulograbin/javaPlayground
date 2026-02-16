f7592656850bb61dc69b1aaad24b80d2fccca96b
Paulo Grabin <paulo.grabin@sap.com>
2026-02-16 12:01:33 -0300

    Reimplement running sum for 1d array for practice

--- src/main/java/com/paulograbin/leetcode/arrays/RunningSumOf1dArray.java
+++ src/main/java/com/paulograbin/leetcode/arrays/RunningSumOf1dArray.java
@@ -3,14 +3,14 @@ package com.paulograbin.leetcode.arrays;
 public class RunningSumOf1dArray {
 
     public int[] runningSum(int[] nums) {
-        int[] sum = new int[nums.length];
+        int[] runningSum = new int[nums.length];
 
-        sum[0] = nums[0];
+        runningSum[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            sum[i] = sum[i-1] + nums[i];
+            runningSum[i] = runningSum[i - 1] + nums[i];
         }
 
-        return sum;
+        return runningSum;
     }
 
 }
